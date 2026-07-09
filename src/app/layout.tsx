add458880c43600c5045a4d5374a9105fcc17244
zaxel <sae11.z.alex@gmail.com>
2026-07-09 15:28:14 +0300

    chore: fix body z-index positioning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} relative -z-10`}>
         <LevaPanel />
         <WixClientContextProvider>
           <Header />
